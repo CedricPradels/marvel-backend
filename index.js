ccb4083959dc6fbf9e0301c1fc97cb401f27eccd
CedricPradels <cedric.pradels@gmail.com>
2020-02-19 14:11:50 +0100

    Minimal research road for characters

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.use(formidableMiddleware());
 // Axios
 const axios = require("axios");
 
+// MARVEL API
+const md5 = require("md5");
+const marvelBaseEndpoint = "https://gateway.marvel.com/";
+const timestamp = 1;
+const hash = md5(
+  `${timestamp}${process.env.MARVEL_KEY_PRIVATE}${process.env.MARVEL_KEY_PUBLIC}`
+);
+
+// CHARACTERS
+// SEARCH
+app.get("/test", async (req, res) => {
+  const path = "/v1/public/characters";
+  const name = "Spider-Man";
+  const orderBy = "name";
+  const limit = "100";
+
+  const response = await axios.get(
+    `${marvelBaseEndpoint}${path}?ts=${timestamp}&apikey=${process.env.MARVEL_KEY_PUBLIC}&hash=${hash}&name=${name}&orderBy=${orderBy}&limit=${limit}`
+  );
+  res.status(200).json(response.data);
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Reçu !!!" });
 });
